a70dddb2a96cf3edbee8a8f3b8227995883a145c
Matey Bogachev <boga4iov.matvei2016@yandex.ru>
2023-05-12 13:07:40 +0400

    class in separate file, logging, graph interface

--- main.py
+++ main.py
@@ -1,202 +1,114 @@
-import os
-from cryptography.hazmat.primitives.asymmetric import rsa, padding as asym_padding
-from cryptography.hazmat.primitives import padding as sym_padding, serialization, hashes
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
-import time
+import re
+import sys
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import (
+    QApplication, QLabel, QMainWindow, QPushButton, QFileDialog)
+from cryptosystem import Hybrid_Cryptosystem
 
 
-class HybridEncryption:
-    def __init__(self):
-        """_Инициализация класса_"""
-        self.backend = default_backend()
-
-    def generate_rsa_keys(self):
+class Window_main(QMainWindow):
+    def __init__(self) -> None:
         """
-        Генерация пары ключей RSA (открытый и закрытый ключ)
-
-        Returns:
-            tuple: private_key, public_key.
+        Initialization function
         """
-        private_key = rsa.generate_private_key(
-            public_exponent=65537, key_size=2048, backend=self.backend)
-        public_key = private_key.public_key()
-        return private_key, public_key
+        super(Window_main, self).__init__()
+        self.setWindowTitle('Camellia')
+        self.setFixedSize(600, 400)
+        self.background = QLabel(self)
+        self.background.setGeometry(0, 0, 600, 400)
+        self.background.setStyleSheet("background-color: #B0E0E6;")
 
-    def rsa_encrypt(self, plaintext: bytes, public_key):
-        """Шифрование текста с использованием алгоритма RSA.
+        self.info = QLabel(self)
+        self.info.setText('Selection size key')
+        self.info.setGeometry(225, 15, 500, 50)
 
-        Args: 
-            plaintext (bytes): текст для шифрования |
-            public_key (RSAPublicKey): открытый ключ RSA.
-        Returns:
-            bytes: зашифрованный текст.
+        self.button_keys = QPushButton('Generate keys', self)
+        self.button_keys.setGeometry(200, 105, 200, 50)
+        self.button_keys.setStyleSheet(
+            ' border-radius: 15%; background-color: #FFFFF0 ;border: 2px solid black;')
+        self.button_keys.clicked.connect(self.generation_key)
+        self.button_keys.hide()
+
+        self.key_size = QtWidgets.QComboBox(self)
+        self.key_size.setStyleSheet(
+            'border-radius: 15%; border: 2px solid black;background-color: #FFFFF0;')
+        self.key_size.addItems(["128 бит", "192 бит", "256 бит"])
+        self.key_size.setGeometry(200, 50, 200, 50)
+        self.key_size.activated[str].connect(self.size_selection)
+
+        self.enycryption_button = QPushButton('Encrypt the text', self)
+        self.enycryption_button.setStyleSheet(
+            ' border-radius: 15%; background-color: #FFFFF0 ;border: 2px solid black;')
+        self.enycryption_button.setGeometry(200, 165, 200, 50)
+        self.enycryption_button.clicked.connect(self.encryption)
+        self.enycryption_button.hide()
+
+        self.decryption_button = QPushButton('Decode the text', self)
+        self.decryption_button.setStyleSheet(
+            ' border-radius: 15%; background-color: #FFFFF0 ; border: 2px solid black;')
+        self.decryption_button.setGeometry(200, 225, 200, 50)
+        self.decryption_button.clicked.connect(self.decryption)
+        self.decryption_button.hide()
+        
+        self.message = QLabel(self)
+        self.message.setGeometry(244, 265, 200, 50)
+        self.show()
+
+    def size_selection(self, text: str) -> None:
         """
-        encrypted = public_key.encrypt(
-            plaintext,
-            asym_padding.OAEP(
-                mgf=asym_padding.MGF1(algorithm=hashes.SHA256()),
-                algorithm=hashes.SHA256(),
-                label=None)
-        )
-        return encrypted
-
-    def rsa_decrypt(self, ciphertext: bytes, private_key):
-        """Расшифровка текста с использованием алгоритма RSA.
-
-        Args: 
-            ciphertext (bytes): зашифрованный текст
-            private_key (RSAPrivateKey): закрытый ключ RSA.
-        Returns:
-            bytes: расшифрованный текст.
+        The function assigns the size of the key
+        Args:
+            text (str): строка которую из которой выбрали нужный ключ
         """
-        decrypted = private_key.decrypt(
-            ciphertext,
-            asym_padding.OAEP(
-                mgf=asym_padding.MGF1(algorithm=hashes.SHA256()),
-                algorithm=hashes.SHA256(),
-                label=None)
-        )
-        return decrypted
+        self.size = int(re.findall('(\d+)', text)[0])
+        self.info.setText("Generate the keys")
+        self.button_keys.show()
 
-    def camellia_encrypt(self, plaintext: bytes, key: bytes, iv: bytes):
-        """Шифрование текста с использованием алгоритма Camellia.
-
-        Args: 
-            plaintext (bytes): текст для шифрования,
-            key (bytes): ключ Camellia,
-            iv (bytes): вектор инициализации.
-        Returns:
-            bytes: зашифрованный текст
+    def generation_key(self) -> None:
         """
-        cipher = Cipher(algorithms.Camellia(
-            key), modes.CBC(iv), backend=self.backend)
-        encryptor = cipher.encryptor()
-
-        padder = sym_padding.PKCS7(128).padder()
-        padded_plaintext = padder.update(plaintext) + padder.finalize()
-
-        ciphertext = encryptor.update(padded_plaintext) + encryptor.finalize()
-        return ciphertext
-
-    def camellia_decrypt(self, ciphertext: bytes, key: bytes, iv: bytes):
-        """Расшифровка текста с использованием алгоритма Camellia.
-
-        Args: 
-            ciphertext (bytes) - зашифрованный текст,
-             key (bytes) - ключ Camellia,
-            iv (bytes) - вектор инициализации.
-        Returns:
-            bytes: расшифрованный текст.
+        The function generates keys and shows 2 buttons(encryption,decryption)
         """
+        way = str(QFileDialog.getExistingDirectory(
+            caption='Selecting a directory'))
+        self.key = Hybrid_Cryptosystem(self.size, way)
+        self.key.generation_key()
+        self.info.setText("Keys generated successfully")
+        self.message.setText("Please encrypt the text")
+        self.decryption_button.show()
+        self.enycryption_button.show()
 
-        cipher = Cipher(algorithms.Camellia(
-            key), modes.CBC(iv), backend=self.backend)
-        decryptor = cipher.decryptor()
+    def encryption(self) -> None:
+        """
+        Encryption function
+        """
+        way_e = str(QFileDialog.getOpenFileName(
+            caption='Select the file for encrypted in txt format', filter='*.txt'))
+        way_e = way_e.split('\'')[1]
+        self.key.encryption(way_e)
+        self.info.setText("The text is encrypted")
+        self.message.setText("Decipher the text")
 
-        padded_plaintext = decryptor.update(ciphertext) + decryptor.finalize()
-
-        unpadder = sym_padding.PKCS7(128).unpadder()
-        plaintext = unpadder.update(padded_plaintext) + unpadder.finalize()
-
-        return plaintext
-
-
-def main():
-    """Выполняет роль меню"""
-    hybrid = HybridEncryption()
-    # Генерация пары ключей RSA
-    private_key, public_key = hybrid.generate_rsa_keys()
-    # Вывод открытого ключа для пользователя
-    pem_public_key = public_key.public_bytes(
-        encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo)
-    print(f"Открытый ключ RSA:\n{pem_public_key.decode('utf-8')}")
-    # Чтение текста из файла
-    input_path = input("Введите путь до файла с текстом: ")
-    while not os.path.isfile(os.path.join(input_path)):
-        input_path = input(
-            "\nА теперь введи, пожалуйста, нормальный путь до файла, а не абракадабру ^_^:")
-    with open(input_path, "rb") as f:
-        plaintext = f.read()
-    # Выбор ключа Camellia
-    key_length = input("\nВыберите длину ключа Camellia (128, 192, 256): ")
-    while (key_length != "128" and key_length != "192" and key_length != "256"):
-        key_length = input(
-            "Нет нет нет так не пойдет... Camellia использует размер ключа 128, 192 или 256 бит так, что выберете один из 3 доспупных: ")
-    key_length = int(key_length)
-    camellia_key = os.urandom(key_length // 8)
-    iv = os.urandom(16)
-    if not os.path.exists("Keys"):
-        os.makedirs("Keys")
-    nametxt = 0
-    while os.path.isfile(os.path.join("Keys", f"Camellia{nametxt}.txt")):
-        nametxt += 1
-    with open(os.path.join("Keys", f"Camellia{nametxt}.txt"), "w") as f:
-        f.write(camellia_key.hex())
-    # Вывод ключа Camellia для пользователя
-    print(f"\nКлюч Camellia (в формате base64) сохранен в папке Keys")
-    # Шифрование текста с использованием Camellia
-    ciphertext = hybrid.camellia_encrypt(plaintext, camellia_key, iv)
-
-    # Шифрование ключа Camellia с использованием RSA
-    encrypted_key = hybrid.rsa_encrypt(camellia_key, public_key)
-
-    # Запрос у пользователя ключа Camellia для расшифровки
-    entered_key_hex = input(
-        "\nВведите ключ Camellia для расшифровки (в формате base64): ")
-    while (entered_key_hex != camellia_key.hex()):
-        entered_key_hex = input("\nНеверный ключ введите еще раз: ")
-    entered_key = bytes.fromhex(entered_key_hex)
-    print("\nОтлично! Ключ принят ожидайте расшифровки текста! ")
-    time.sleep(2)
-    # Расшифровка ключа Camellia с использованием RSA
-    decrypted_key = hybrid.rsa_decrypt(encrypted_key, private_key)
-
-    decrypted_text = hybrid.camellia_decrypt(ciphertext, entered_key, iv)
-
-    if plaintext == decrypted_text:
-        print("\nШифрование и расшифровка прошли успешно!")
-    else:
-        print("\nОшибка в процессе шифрования и/или расшифровки.")
-
-    # Сохранение зашифрованного текста в файл
-    name_path_ciphertext = input(
-        "\nВведите имя для файла для зашифрованного текста: ")
-    if not os.path.exists("Ciphertext"):
-        os.makedirs("Ciphertext")
-    with open(os.path.join("Ciphertext", f"{name_path_ciphertext}.txt"), "wb") as f:
-        f.write(ciphertext)
-    # Сохранение расшифрованного текста в файл
-    if plaintext == decrypted_text:
-        if not os.path.exists("Decrypted"):
-            os.makedirs("Decrypted")
-        decrypted_output_path = input(
-            "\nВведите имя для файла для расшифрованного текста: ")
-        with open(os.path.join("Decrypted", f"{decrypted_output_path}.txt"), "wb") as f:
-            f.write(decrypted_text)
-    else:
-        print("\nУпс произошла ошибка, что маловероятно, наверное мой код не идеален, что так же маловероятно, или же сегодня ретроградный меркурий другого не дано *_*")
+    def decryption(self) -> None:
+        """
+        Decoding function
+        """
+        way = self.key.decryption()
+        self.info.setText("The text decrypted successfully")
+        self.message.setGeometry(155, 280, 600, 30)
+        self.message.setText(
+            f"\tThe decrypted text is located at this path:\n{way}")
 
 
-def menu():
-    """Повторюшка"""
-    flag = True
-    while (flag):
-        main()
-        ro = input(
-            "\nХотите еще раз поиграться с данной программой? да/нет yes/no : ")
-        while (ro != "yes" and ro != "no" and ro != "да" and ro != "нет"):
-            ro = input(
-                "\nохххх я же спросил да или нет зачем же писать что-то друое... -_-\nХорошо давайте по новой. да или нет (:  ")
-        if (ro == "no" or ro == "нет"):
-            flag = False
-            print("\nНа нет и суда нет O_o")
-            time.sleep(1)
-        else:
-            print("\nДело сделано... опять работа ?")
-            time.sleep(2)
-    print("\nКто прочитал у того мама будет жить вечно!")
+def application() -> None:
+    """
+    The function starts the application
+    """
+    app = QApplication(sys.argv)
+    window = Window_main()
+    window.show()
+    sys.exit(app.exec_())
 
 
 if __name__ == "__main__":
-    menu()
+    application()
